3d8125bfb9055a3d49a17a4133e6d5f5d8f811a1
Amanpandeya44 <ap5766708@gmail.com>
2026-01-28 18:24:06 +0530

    some new code

--- project/myapp/views.py
+++ project/myapp/views.py
@@ -6,4 +6,17 @@ def landing(req):
     return render(req,'landing.html')
 
 def register(req):
+    print("hello......")
+    print(req.POST)
+    print(req.FILES)
+    if req.method=='POST':
+        n = req.POST.get('name')
+        e = req.POST.get('email')
+        p = req.POST.get('password')
+        cp = req.POST.get('cpassword')
+        i = req.FILES.get('image')
+        a = req.FILES.get('audio')
+        v = req.FILES.get('video')
+        d = req.FILES.get('resume')
+        print(n,e,p,cp,i,a,v,d,sep=',')
     return render(req,'register.html')
