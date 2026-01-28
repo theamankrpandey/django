0ec337962f49e178ead60e257fbf4d92694fb5ad
Amanpandeya44 <ap5766708@gmail.com>
2026-01-28 17:07:46 +0530

    some new code

--- project/project/urls.py
+++ project/project/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from myapp.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('landing/',landing,name='landing'),
     path('register/',register,name='register'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
