160de183c9534e61179d72636702aa3cebfeb3a7
Amanpandeya44 <ap5766708@gmail.com>
2026-01-30 17:27:37 +0530

    some new code

--- project/project/urls.py
+++ project/project/urls.py
@@ -22,6 +22,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('landing/',landing,name='landing'),
+    path('',landing,name='landing'),
     path('register/',register,name='register'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
