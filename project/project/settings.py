658246ee27f23f08c59a9be40b026fb0cdca061d
Amanpandeya44 <ap5766708@gmail.com>
2026-02-04 17:35:35 +0530

    new code

--- project/project/settings.py
+++ project/project/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3 ',
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME":'pythondb',
+        "USER":'root',
+        "PASSWORD":'aman123',
+        "POST":3306
     }
 }
 
